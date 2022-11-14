4fc602d6e318d204e8fe599082ef2663c9dc80d0
adelyanasretdinova <adelya.nasretdinova@gmail.com>
2022-11-14 20:11:12 +0100

    added authentification

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "awsintroamplifyapp7a591939": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
